d278826af63e2d96777925912de38b2b4289eac8
Jhimson <jhimsonrayraboypamisa@gmail.com>
2020-06-08 22:23:37 -0400

    Dashboard layout

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,10 +1,122 @@
 import React from "react";
 import { useContext } from "react";
 import { GlobalContext } from "../context/GlobalContext";
+import "./dashboard.css";
 
 const Dashboard = () => {
   const { name } = useContext(GlobalContext);
-  return <h1>{name}</h1>;
+  return (
+    <div>
+      <div className="navbar">
+        <h1 className="title-bar">Project Manager</h1>
+      </div>
+      <div className="container">
+        <div className="sidebar">
+          <nav>
+            <ul className="project-list">
+              <div className="sidebar-header">
+                <h1 className="sidebar-header-title">Projects</h1>
+                <button className="add-new-project-btn">+</button>
+              </div>
+              <li className="project-item">
+                <button className="project-item-btn">Grammar Exam App</button>
+              </li>
+              <li className="project-item">
+                <button className="project-item-btn">Ticketing System</button>
+              </li>
+              <li className="project-item">
+                <button className="project-item-btn">Grammar Exam App</button>
+              </li>
+              <li className="project-item">
+                <button className="project-item-btn">Grammar Exam App</button>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div className="main">
+          <div className="project-name-box">
+            {/* <input type="text" value={"Grammar Exam App"} /> */}
+            <h1 className="project-name-box-title">Grammar Exam App</h1>
+            <button>+</button>
+            <button>x</button>
+          </div>
+
+          <div className="project-task-list">
+            <div className="task-list-item">
+              <div className="task-list-item-navbar">
+                <h1 className="task-list-item-navbar-title">New Task</h1>
+                <button>+</button>
+                <button>x</button>
+              </div>
+            </div>
+            <div className="task-list-item">
+              <div className="task-list-item-navbar">
+                <h1 className="task-list-item-navbar-title">New Task</h1>
+                <button>+</button>
+                <button>x</button>
+              </div>
+            </div>
+            <div className="task-list-item">
+              <div className="task-list-item-navbar">
+                <h1 className="task-list-item-navbar-title">New Task</h1>
+                <button>+</button>
+                <button>x</button>
+              </div>
+            </div>
+            <div className="task-list-item">
+              <div className="task-list-item-navbar">
+                <h1 className="task-list-item-navbar-title">New Task</h1>
+                <button>+</button>
+                <button>x</button>
+              </div>
+            </div>
+            <div className="task-list-item">
+              <div className="task-list-item-navbar">
+                <h1 className="task-list-item-navbar-title">New Task</h1>
+                <button>+</button>
+                <button>x</button>
+              </div>
+            </div>
+            <div className="task-list-item">
+              <div className="task-list-item-navbar">
+                <h1 className="task-list-item-navbar-title">New Task</h1>
+                <button>+</button>
+                <button>x</button>
+              </div>
+            </div>
+            <div className="task-list-item">
+              <div className="task-list-item-navbar">
+                <h1 className="task-list-item-navbar-title">New Task</h1>
+                <button>+</button>
+                <button>x</button>
+              </div>
+            </div>
+            <div className="task-list-item">
+              <div className="task-list-item-navbar">
+                <h1 className="task-list-item-navbar-title">New Task</h1>
+                <button>+</button>
+                <button>x</button>
+              </div>
+            </div>
+            <div className="task-list-item">
+              <div className="task-list-item-navbar">
+                <h1 className="task-list-item-navbar-title">New Task</h1>
+                <button>+</button>
+                <button>x</button>
+              </div>
+            </div>
+            <div className="task-list-item">
+              <div className="task-list-item-navbar">
+                <h1 className="task-list-item-navbar-title">New Task</h1>
+                <button>+</button>
+                <button>x</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
